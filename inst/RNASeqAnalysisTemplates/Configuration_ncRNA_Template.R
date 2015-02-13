32c70b9ffbf5701e590290054b1c56160c9de9da
SimonSchafferer <schafferer.simon@gmail.com>
2015-02-13 13:50:03 +0100

    added perlScriptFor fasta modification

--- inst/RNASeqAnalysisTemplates/Configuration_ncRNA_Template.R
+++ inst/RNASeqAnalysisTemplates/Configuration_ncRNA_Template.R
@@ -13,21 +13,22 @@ rootDir = file.path( "/home","simon","PHDStudies","RNA-Seq","IonProton","Alzheim
 #Directory containging fastq files
 rawDataDir = file.path(rootDir,"rawData")
 
-#Path to rna star mapper Index file for whole genome
+#Path to rna star mapper Index file for whole genome (download genome fasta file from UCSC -> twoBit format -> convert with twoBitToFa )
 genomeIndexFilePath = "/home/simon/dbsOfflineUse/HomoSapiens/hg19/rnaStarIndex"
 
-#Path to rna star mapper Index file for ncRNAs
+#Path to rna star mapper Index file for ncRNAs -> Please use RNASeqUtility::createSizeSelectedEnsemblncRNADB method! *see Prepare ncRNAIndex below
 genomeIndexFilePath_ncRNA = "/home/simon/dbsOfflineUse/HomoSapiens/hg19/ncRNA_ENSEMBL/restr400nt_extended"#please uncomment above if a db dir has to be created
 
-#For the annotation Script
-
+##############
+#     For the annotation Script
+##############
 #Path to infernal database (compiled)
 infernalDB = file.path("/home","simon","dbsOfflineUse","HomoSapiens","hg19","infernal","Rfam.cm.1_1")
 
-#Path to repeat masker file
+#Path to repeat masker file (Please see below for further information)
 repeatMaskerDir = "/home/simon/dbsOfflineUse/HomoSapiens/hg19/repeatMskr"
 #Name of the repeat masker file (either .rda (see sncRNAannotation package) or bed file!)
-repeatMaskerFN = "rmsk_hg19.bed"
+repeatMaskerFN = "rmsk_hg19.bed" #or GRanges object
 
 #Assembly of Annotation:
 organismForAnnotation = "hg19" #
@@ -37,9 +38,9 @@ organismForAnnotation = "hg19" #
 ######################################################
 #              Default parameters
 ######################################################
-grouping = TRUE #This flag defines if the groups should be considered when intersecting all contigs...
-withinGroupTH = 0 #This is the threshold for contig assembly within the groups, it defines how many samples of a group do not need to have reads for this contig
-read_threshold = 1 # read threshold for contig assembly
+grouping = TRUE #This flag defines if the groups should be considered when intersecting all contigs... *see Contig Assembly below
+withinGroupTH = 0 #This is the threshold for contig assembly within the groups, it defines how many samples of a group do not need to have reads for this contig *see Contig Assembly below
+read_threshold = 1 # read threshold for contig assembly 
 readOverlap_contig = -1 #This parameters defines the number of nt a read has to overlap with the next in order to be clustered (from positive to negative numbers -> APART default: 0)
 readCompositionIdentity = 0.95 #This is used for clustering: If 0.95: At least 95% of the reads have to be shared between a contig with the representative contig (highest read count) in order to be clustered, 
 # so in this case if there are 5% reads that are different then the contig will be kept next to the representative contig. When this value is set to 1 then contigs will be clustered to the 
@@ -133,15 +134,17 @@ dir.create(diffExpDir)
 samplesInfo = read.table(file=file.path(rootDir, "samplesInfo.csv"), sep="\t", header=TRUE) #should contain cloumn condition and column sample name
 if(!( "sampleName" %in% colnames(samplesInfo) & "condition"%in% colnames(samplesInfo))) {stop("Please provide a sampleInfo file with column condition and column sampleName")}
 
-
+#Saving the configuration file: DO NOT CHANGE ITS NAME!
+save.image(file.path(rootDir,"Configuration.rda") )
 
 ####################################################################################################################################
 #                                         Additional Infos
 #####################################################################################################################################
 
 #############################################
-#     Contig Assembly - among samples - MultiIntersectBed
-#   Now a PERL script I have written is doing the multi intersection for plus and minus strand bed files -> and merging them. 
+#           Contig Assembly - among samples - MultiIntersectBed
+#
+#   PERL script is doing the multi intersection for plus and minus strand bed files -> and then merging them. 
 #
 #   First usecase: 
 #     Basically, if no groups are defined then it uses multiInterSect Bed build in cluster algorithm that searches for an overlap in all samples and only reports the 
@@ -165,44 +168,47 @@ if(!( "sampleName" %in% colnames(samplesInfo) & "condition"%in% colnames(samples
 
 
 
-##############################################
-# DOWNLOAD THE ncRNA fasta file from ENSEMBL
-# SUBSET THIS FILE BY THE MAXIMUM READ LENGTH -> in our case 200
-# bring the file into the right format (no line breaks if there are!)
-#/home/simon/dbsOfflineUse/MusMusculus/Mouse_ncRNA_fasta/reformat_fasta.pl
-#or python script EliminateNewLinesFasta.py
-#/home/simon/dbsOfflineUse/MusMusculus/Mouse_ncRNA_fasta/Mus_musculus.GRCm38.ncrna.mod.fa
-# awk '!/^>/ { next } { getline seq } length(seq) <= 200 or 400 for size selection { print $0 "\n" seq }' FastaFileInput.fa > FastaFileOutput.fa
-# create genome index file 
-# STAR --runThreadN 5 --runMode genomeGenerate --genomeDir /home/simon/dbsOfflineUse/MusMusculus/Mouse_ncRNA_fasta/sizeRestricted --genomeFastaFiles /home/simon/dbsOfflineUse/MusMusculus/Mouse_ncRNA_fasta/sizeRestricted/Mus_musculus.GRCm38.ncrna_sizeRestricted.fa
-# Map all reads to this artificial genome containing ncRNAs only!
-# proceed with unmapped reads as follows
-# This pipeline may be run on unmapped reads from primary mapping
-# STAR --genomeDir /home/simon/dbsOfflineUse/MusMusculus/Mouse_ncRNA_fasta --readFilesIn /home/simon/PHDStudies/RNA-Seq/IonProton/CavKO_Striessnig/MouseBrain_wo_Ref/rawData/test/IonXpressRNA_001.R_2014_08_22_11_48_07_user_PRO-36-2014-08-22_MoBrain_Ion_RNA_-_20-200nt_Transcriptom.fastq --outFileNamePrefix test1 --runThreadN 8 --outFilterMismatchNoverLmax 0.05 --outFilterMatchNmin 16 --outFilterScoreMinOverLread 0 --outFilterMatchNminOverLread 0 --alignIntronMax 1 --outFilterMultimapNmax 100 --outSAMprimaryFlag AllBestScore --outReadsUnmapped Fastx
+#############################################
+#     Prepare ncRNA index
+#
+#   Call method
+#   RNASeqUtility::createSizeSelectedEnsemblncRNADB
+#
+#   if one wants to add other annotations next to ensembl please call this method with generateIdx = FALSE
+#   Then prepare your files (for example fasta from mirbase) use: PreparencRNAfasta.py 
+#   Script can be found by: file.path( system.file( package="RNASeqUtility"), "data", "PreparencRNAfasta.py")
+#   This would be an example to change a fasta containing Us to Ts and replacing whiteSpaces in Ids with _ and replacing newLines in the sequences
+#   PreparencRNAfasta.py -i testfasta.fa -o testfastaMod.fa --rmSpaceInID T --replacementChar '_' --RNAtoDNA T
+#
+#   Maybe adding the ncRNA class to the header with sed: 
+#   sed -i 's/>/>snoRNA_hacabox_/g' HacaBox.fa
+#
+#   adding the file to the ENSEMBL ncRNA fasta file
+#   cat <PREPARED NCRNA CLASS FASTA> >> <MODIFIED ENSEMBL NCRNA FASTA>
+#
+#   Now building the genome index with STAR 
+#   STAR --runThreadN <NR> --runMode genomeGenerate --genomeDir <DIR>  --genomeFastaFiles <FASTA>
+#
+#   The resulting path of the index directory is then used for the variable: genomeIndexFilePath_ncRNA
+#############################################
+
+#############################################
+#     Prepare repeat masker data
+#   Download repeats masker bed file from UCSC (Table Browser) for the reuqired organism
 # 
-# # --outSAMtype BAM SortedByCoordinate
-# 
-# samtools view -bS test1Aligned.out.sam > test1Aligned.out.bam
-# bamToBed -i test1Aligned.out.bam > test1Aligned.out.bed
-# sort -k1,1 -k2,2n test1Aligned.out.bed | mergeBed -s -c 4,5,6 -o count,mean,distinct -i stdin | awk '{if($4 > 5) print }'> test1Aligned.out.merged.bed
-###############################################
-# STAR --genomeDir /home/simon/dbsOfflineUse/MusMusculus/Mouse_ncRNA_fasta/sizeRestricted --readFilesIn /home/simon/PHDStudies/RNA-Seq/IonProton/CavKO_Striessnig/MouseBrain_wo_Ref/rawData/test/IonXpressRNA_001.R_2014_08_22_11_48_07_user_PRO-36-2014-08-22_MoBrain_Ion_RNA_-_20-200nt_Transcriptom_trimmed.fastq --outFileNamePrefix test1 --runThreadN 8 --outFilterMismatchNoverLmax 0.05 --outFilterMatchNmin 16 --outFilterScoreMinOverLread 0 --outFilterMatchNminOverLread 0 --alignIntronMax 1 --outFilterMultimapNmax 100 --outSAMprimaryFlag AllBestScore --outReadsUnmapped Fastx --outSAMtype BAM Unsorted
-
-#samtools view -bS test1Aligned.out.sam > test1Aligned.out.bam
-# bamToBed -i test1Aligned.out.bam > test1Aligned.out.bed
-# sort -k1,1 -k2,2n test1Aligned.out.bed | mergeBed -s -c 4,5,6 -o count,mean,distinct -i stdin | awk '{if($4 > 5) print }'> test1Aligned.out.merged.bed
-
-# wc -l test1Aligned.out.bed 14029759 reads
-# wc -l test1Aligned.out.merged.bed 1368 ncRNAs
-# some stats
-#The names need to be modified to provide unique IDs:  _se or _as
-# ENSMUST00000082825  31  65	6	0	+
-# ENSMUST00000082825	96	169	14	183	-
-# in the end get the coordinates and names from biomart: EnsemblGeneID, EnsemblTranscriptID, GeneStart, GeneEnd, ChromosomeName, Strand, MGIsymbol, MGItranscriptname
-# MGI symbol and transcript name may be employed for annotation clustering!
-
-# with all unmapped reads perform the following script...
+#   Optional, if more information is wanted, download from UCSC table browser and select fields from 'Select fields from primary and related table'
+#   In this case the table has to be read into R and also changed to GRanges object accordingly
+#   Read table
+#   rpmsk = read.table( "<PATHTOTABLE>" )
+#   convert to GRanges object
+#   rmpsk_mm10 = with(rpmsk, GRanges( seq=<COLUMN>, IRanges(<COLUMN>,<COLUMN>), strand=<COLUMN>, ... ))
+#   save object
+#   save(rpmsk_mm10, file="<PATHTOFILE>")
+#############################################
 
 
-#Saving the configuration file: DO NOT CHANGE ITS NAME!
-save.image(file.path(rootDir,"Configuration.rda") )
+#############################################
+#         Prepare annotation for sncRNAannotation
+# call sncRNAannotation::createRObject_gtf function, or execute the steps in this function
+#############################################
+
